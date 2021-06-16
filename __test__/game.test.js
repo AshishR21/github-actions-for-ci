9cd5e2a9398102bcebef2e420c3151b0164f6c52
Ashish Ranjan <ash.ran.ranjan1122@gmail.com>
2021-06-16 12:13:44 +0530

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,7 +2,7 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('App Contains the compiled JavaScript 1', async () => {
+  it('Contains the compiled JavaScript', async () => {
     const data = fs.readFileSync('./public/main.js', 'utf8')
     expect(data).toMatchSnapshot()
   })
